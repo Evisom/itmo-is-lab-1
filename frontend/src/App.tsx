0c8bdbad50ce8e46e17ae674c1065a466f71dd86
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2024-10-04 14:26:25 +0300

    appbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,19 @@ import { RootState } from "./store/store"; // Import the RootState type
 import { setUsername, setToken } from "./store/userSlice"; // Import your actions
 import "./App.scss";
 import { redirect, useNavigate } from "react-router-dom";
+import AddIcon from "@mui/icons-material/Add";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
+import {
+  AppBar,
+  Button,
+  Container,
+  Icon,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -12,11 +25,13 @@ const App = () => {
   const username = useSelector((state: RootState) => state.user.username);
   const token = useSelector((state: RootState) => state.user.token);
 
+  const isAdmin = true;
+
   useEffect(() => {
     const lsToken = localStorage.getItem("token");
     const lsUsername = localStorage.getItem("username");
     if (!(token || lsToken)) {
-      // navigate("/login");
+      navigate("/login");
     }
     if (lsToken && !token) {
       dispatch(setToken(lsToken + ""));
@@ -36,6 +51,36 @@ const App = () => {
 
   return (
     <div className="App">
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            IS-LAB-1
+          </Typography>
+          <div
+            style={{
+              display: "flex",
+              gap: 24,
+              alignItems: "center",
+            }}
+          >
+            <Button color="inherit" variant="outlined">
+              Создать
+              <AddIcon />
+            </Button>
+            {isAdmin && <Button color="inherit">админка</Button>}
+            <div style={{ display: "flex", gap: 8 }}>
+              <AccountCircleIcon />
+              <Typography>{username}</Typography>
+            </div>
+            <LogoutIcon
+              onClick={() => {
+                localStorage.clear();
+                dispatch(setToken(""));
+              }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
       hello, {username}, token: {token}
     </div>
   );
